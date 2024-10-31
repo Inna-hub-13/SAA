cf8cb6f58d41e900a9d4a40f32e886a1a1c39a29
Inna <ogsingerlan13@gmail.com>
2024-10-31 12:34:34 +0200

    Add files via upload

---
+++ Upr4/Gifts.c
@@ -0,0 +1,115 @@
+
+// Assume there are N gifts. Each gift is represented by intger value, 
+// stored in an array.
+// Separate the gifts among two people, so that they have equal or almost equal
+// value of presents.
+
+// Dynamic programminc
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+int sum(int*, int);
+void separate_gifts(int*, int);
+void separate_gifts2(int*, int);
+
+int main() {
+
+    int gifts[N] = {3, 4, 5, 5, 1};
+    separate_gifts(gifts, N);
+    separate_gifts2(gifts, N);
+
+    return 0;
+}
+
+int sum(int* arr, int size) {
+
+    int sum = 0, i;
+
+    for(i = 0; i < size; i++)
+        sum += *(arr + i);
+    
+    return sum;
+}
+
+void separate_gifts(int* gifts, int size) {
+
+    int total = sum(gifts, size);
+
+    // sums -> represents all the possible sums of values of gifts
+    // said in other words - all the possibilities to separate the gifts,
+    // where 1 - there is such combination of gifts, 0 there is no such combination
+    // each index of sums == sum 
+    int* sums = (int*)malloc((total + 1) * sizeof(int));
+    if(!sums)
+        return;
+
+    int i, j;
+    *sums = 1;
+    // initialize the rest of the sums array with zeros
+    for(i = 1; i < total + 1; i++)
+        *(sums + i) = 0;
+    
+    // starting from the end of the sums array
+    // look for ones to make more combinations
+    for(j = 0; j < size; j++)
+        for(i = total; i >= 0; i--)
+            if(*(sums + i) == 1)
+                *(sums + i + *(gifts + j)) = 1;
+    
+    for(j = 0; j < total + 1; j++)
+        printf("%d ", *(sums + j));
+
+    // find the middle of sums array to seprate fairly
+    j = total / 2;
+    while(j == 0)
+        j--;
+    
+    printf("\nGift value for the first %d\n", j);
+    printf("Gift value for the seconds %d\n", total - j);
+}
+
+
+void separate_gifts2(int* gifts, int size) {
+
+    int total = sum(gifts, size);
+
+    // sums -> represents all the possible sums of values of gifts
+    // said in other words - all the possibilities to separate the gifts,
+    // where 1 - there is such combination of gifts, 0 there is no such combination
+    // each index of sums == sum 
+    int* sums = (int*)malloc((total + 1) * sizeof(int));
+    if(!sums)
+        return;
+
+    int i, j;
+
+    *sums = 1;
+    // initialize the rest of the sums array with zeros
+    for(i = 1; i < total + 1; i++)
+        *(sums + i) = 0;
+
+    for(j = 0; j < size; j++)
+        for(i = total; i >= 0; i--)
+            if(*(sums + i) != 0)
+                *(sums + i + *(gifts + j)) = *(gifts + j);
+
+    for(j = 0; j < total + 1; j++)
+        printf("%d ", *(sums + j));
+
+    printf("\n");
+    j = total / 2;
+
+    while(j == 0)
+        j--;
+
+    printf("medium: %d\n", j);
+    
+    printf("Gifts  for the first: ");
+    for(i = j; i > 0; i -= *(sums + i))
+        printf("%d ", *(sums + i));
+
+    printf("\n");
+    printf("The other gifts are for the second\n");
+}
