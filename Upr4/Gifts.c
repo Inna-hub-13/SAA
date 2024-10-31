8c7e2f2834363493a0b56e028a6b7715ca11ad27
Inna <ogsingerlan13@gmail.com>
2024-10-31 12:35:22 +0200

    Update Gifts.c

--- Upr4/Gifts.c
+++ Upr4/Gifts.c
@@ -12,13 +12,11 @@
 
 int sum(int*, int);
 void separate_gifts(int*, int);
-void separate_gifts2(int*, int);
 
 int main() {
 
     int gifts[N] = {3, 4, 5, 5, 1};
     separate_gifts(gifts, N);
-    separate_gifts2(gifts, N);
 
     return 0;
 }
@@ -68,48 +66,4 @@ void separate_gifts(int* gifts, int size) {
     
     printf("\nGift value for the first %d\n", j);
     printf("Gift value for the seconds %d\n", total - j);
-}
-
-
-void separate_gifts2(int* gifts, int size) {
-
-    int total = sum(gifts, size);
-
-    // sums -> represents all the possible sums of values of gifts
-    // said in other words - all the possibilities to separate the gifts,
-    // where 1 - there is such combination of gifts, 0 there is no such combination
-    // each index of sums == sum 
-    int* sums = (int*)malloc((total + 1) * sizeof(int));
-    if(!sums)
-        return;
-
-    int i, j;
-
-    *sums = 1;
-    // initialize the rest of the sums array with zeros
-    for(i = 1; i < total + 1; i++)
-        *(sums + i) = 0;
-
-    for(j = 0; j < size; j++)
-        for(i = total; i >= 0; i--)
-            if(*(sums + i) != 0)
-                *(sums + i + *(gifts + j)) = *(gifts + j);
-
-    for(j = 0; j < total + 1; j++)
-        printf("%d ", *(sums + j));
-
-    printf("\n");
-    j = total / 2;
-
-    while(j == 0)
-        j--;
-
-    printf("medium: %d\n", j);
-    
-    printf("Gifts  for the first: ");
-    for(i = j; i > 0; i -= *(sums + i))
-        printf("%d ", *(sums + i));
-
-    printf("\n");
-    printf("The other gifts are for the second\n");
 }
