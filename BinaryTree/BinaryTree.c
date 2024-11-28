9293c94f6ec6098254f0d0217cb2833b58a78a0b
Inna <ogsingerlan13@gmail.com>
2024-11-28 10:07:40 +0200

    Update BinaryTree.c

--- BinaryTree/BinaryTree.c
+++ BinaryTree/BinaryTree.c
@@ -1,5 +1,7 @@
 // Some functions for binary tree
 
+// 28.11 2024 - more functions to add, in progress
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,10 +12,23 @@ struct node{
     struct node* right;
 };
 
+struct stack{
+
+    struct node n;
+    struct stack* prev;
+};
+
 void create_tree(struct node**, char*);
 void preorder(const struct node*);
+void preorder_iteration(const struct node*);
 void inorder(const struct node*);
 void postorder(const struct node*);
+int power(const struct node*);
+int max_el(int, int);
+int depth(const struct node*);
+
+int push(struct stack**, struct node*);
+int pop(struct stack**, struct node*);
 
 int main() {
 
@@ -107,4 +122,56 @@ void postorder(const struct node*root) {
         postorder(root->right);
         printf("%c ", root->value);
     }
+}
+
+// counts number of nodes of the binary tree
+int power(const struct node* root) {
+
+    if(root == NULL)
+        return 0;
+    
+    return 1 + power(root->left) + power(root->right);
+}
+
+int max_el(int a, int b) {
+
+    return a > b ? a : b;
+}
+
+int depth(const struct node* root) {
+
+    if(root == NULL)
+        return 0;
+    
+    return 1 + max_el(depth(root->left), depth(root->right));
+}
+
+int push(struct stack** top, struct node* n) {
+
+    struct stack* temp = (struct stack*)malloc(sizeof(struct stack));
+    if(!temp)
+        return -1;
+
+    temp -> prev = *top;
+    *top = temp;
+    temp->n = *n;
+    return 0;
+}
+
+int pop(struct stack** top, struct node* n) {
+
+    if(!(*top))
+        return NULL;
+
+    struct stack* temp = *top;
+    *top = temp->prev;
+    *n = temp->n;
+    free(temp);
+    return 0;
+}
+
+void preorder_iteration(const struct node* root) {
+
+    struct stack* top = NULL;
+    
 }
