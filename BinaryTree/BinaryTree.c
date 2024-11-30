7d25909297255b0b953cba67baf3dcdc538a78dc
Inna <ogsingerlan13@gmail.com>
2024-11-30 12:38:14 +0200

    Update BinaryTree.c

--- BinaryTree/BinaryTree.c
+++ BinaryTree/BinaryTree.c
@@ -177,7 +177,7 @@ int pop(struct stack** top, struct node** node) {
 void preorder_iteration(struct node* root) {
 
     struct stack* top = NULL;
-    struct node* temp_node;
+    struct node* temp_node = NULL;
 
     if(root)
         push(&top, root);
